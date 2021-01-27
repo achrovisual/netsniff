d242071ba807840d96c0da5b1dfc779cc2578e68
RafMdrzo <rafael_maderazo@dlsu.edu.ph>
2021-01-28 01:55:19 +0800

    added file writing for connected nodes

--- netsniff.py
+++ netsniff.py
@@ -1,6 +1,13 @@
+import signal
+import socket, sys, os, re, time, netifaces
 from scapy.all import *
 import ipaddress
-import socket
+import collections
+
+try:
+    collectionsAbc = collections.abc
+except AttributeError:
+    collectionsAbc = collections
 
 def arp_scan(ip):
     try:
@@ -8,25 +15,26 @@ def arp_scan(ip):
 
         ans, unans = srp(request, timeout=2, retry=1)
         result = []
-
+        f = open("MAC-IP Mapping.txt", "w")
+        f.write("Connected Nodes:\n")
         for sent, received in ans:
             client = {'IP': received.psrc, 'MAC': received.hwsrc}
+            f.write(str(client) + "\n")
             print(client)
             result.append(client)
+        f.close()
         return result
     except KeyboardInterrupt as e:
         sys.exit(0)
 
-if __name__ == '__main__':
-    try:
-        # host_name = socket.gethostname()
-        # host_ip = socket.gethostbyname(host_name)
 
-        # ip = ipaddress.ip_interface(host_ip + '/0.0.0.0')
-        # ip = ipaddress.ip_interface('172.16.0.0/255.240.0.0')
+try:
+    # host_name = socket.gethostname()
+    # host_ip = socket.gethostbyname(host_name)
 
-        ip = input('Enter network address: ')
-
-        print(arp_scan(ip))
-    except KeyboardInterrupt as e:
-        sys.exit(0)
+    # ip = ipaddress.ip_interface(host_ip + '/0.0.0.0')
+    # ip = ipaddress.ip_interface('172.16.0.0/255.240.0.0')
+    ip = input('Enter network address: ')
+    print(arp_scan(ip))
+except KeyboardInterrupt as e:
+    sys.exit(0)
