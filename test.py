5f0f84a86a4f5cf3d0b7300a4e26bb6bf5ac8fbf
RafMdrzo <rafael_maderazo@dlsu.edu.ph>
2021-01-29 23:18:25 +0800

    cleaned up http sniffing.  threading of sniff function needed,

--- test.py
+++ test.py
@@ -24,27 +24,37 @@ def TCPPrint(pkt):
 def HTTPPrint(pkt):
     httppkt = str(pkt)
     global count
-    if IP in pkt:
-        ipSrc =pkt[IP].src
-        ipDst =pkt[IP].dst
-    if TCP in pkt:
-        tcpSrc = pkt[TCP].sport
-        tcpDst = pkt[TCP].dport
-    print("IP SRC " + str(ipSrc) + " || TCP SRC PORT " + str(tcpSrc))
-    print("IP DST " + str(ipDst) + " || TCP DST PORT " + str(tcpDst))
-    print("==========================================================")
+    ipSrc = ""
+    ipDst = ""
+    tcpSrc = ""
+    tcpDst = ""
+    try:
+        if IP in pkt:
+            ipSrc =pkt[IP].src
+            ipDst =pkt[IP].dst
+        if TCP in pkt:
+            tcpSrc = pkt[TCP].sport
+            tcpDst = pkt[TCP].dport
+        print("IP SRC " + str(ipSrc) + " || TCP SRC PORT " + str(tcpSrc))
+        print("IP DST " + str(ipDst) + " || TCP DST PORT " + str(tcpDst))
+        print("==========================================================")
 
-    if httppkt.find('GET'):
-        count+=1
-    elif httppkt.find('PUT'):
-        count+=1
-    elif httppkt.find('POST'):
-        count+=1
-    elif httppkt.find('DELETE'):
-        count+=1
-    elif httppkt.find('PATCH'):
-        count+=1
-
-choice = input("Filter: [1] HTTP, [2] TCP, [3] ARP, [4] DHCP, [5] UDP")
-y = sniff(count = 0, timeout=10, filter = "tcp", prn = HTTPPrint) #test function
-print("Count: " + str(count))
+        if httppkt.find('GET'):
+            count+=1
+        elif httppkt.find('PUT'):
+            count+=1
+        elif httppkt.find('POST'):
+            count+=1
+        elif httppkt.find('DELETE'):
+            count+=1
+        elif httppkt.find('PATCH'):
+            count+=1
+    except Exception as e:
+        print("Count: " + str(count))
+        print(e)
+#choice = input("Filter: [1] HTTP, [2] TCP, [3] ARP, [4] DHCP, [5] UDP\nChoice:\t")
+try:
+    y = sniff(count = 0, filter = "http", prn = HTTPPrint) #test function
+except KeyboardInterrupt as e:
+    print(e)
+    print("Count: " + str(count))
