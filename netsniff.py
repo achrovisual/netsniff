813f578f47a2326538fc547a7290c52d01b17a24
Eugenio Pastoral <stonenobrein@gmail.com>
2021-02-12 12:09:37 +0800

    Added comments and implemented argument parsing

--- netsniff.py
+++ netsniff.py
@@ -1,56 +1,88 @@
+# Author: Rafael Maderazo and Eugenio Pastoral
+# Course: Data Communications
+
+import ipcalc, sniffer, argparse, sys, netifaces
 from datetime import datetime
-import ipcalc
-import sniffer
-import signal
-import socket, sys, os, re, time, netifaces
 from scapy.all import *
-import ipaddress
-import collections
 
 try:
-    collectionsAbc = collections.abc
-except AttributeError:
-    collectionsAbc = collections
+    from scapy.all import *
+except ImportError:
+    print("Scapy library for Python is not installed on your system. Run 'pip install --pre scapy[basic]' to install the library.")
+    print("For more information, visit https://scapy.readthedocs.io/en/latest/installation.html to isntall Scapy.")
+    exit(0)
 
+# Declare sniffer object
 sniff = sniffer.Sniffer()
 
+# Get current date and time for the filename of the dump file
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%d %H.%M.%S")
 filename = "netsniff_dump " + dt_string + ".txt"
 
+# This function scans the network through sending ARP packets. The result is the IP:MAC mapping of the devices in the network. It takes in an IPv4 address as a parameter.
 def arp_scan(ip):
     try:
+        # Print console message.
         print("[*] Scanning " + ip + "...")
-        request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
 
+        # Initialize the packet.
+        request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
+        # Send out the packet.
         ans, unans = srp(request, timeout=2, iface='ens18', inter=0.1, verbose = 0)
+
+        # Initialize result list.
         result = []
+
+        # Create the dump file.
         f = open(filename, "w")
         f.write("****IP:MAC Mapping****\n")
+
+        # Get the IPv4 and MAC addresses in each response packet.
         for sent, received in ans:
+            # Write the IPv4 and MAC addresses into the dump file.
+            f.write("IP: " + str(received.psrc) + " | MAC: " + str(received.hwsrc) + "\n")
+
+            # Store the IPv4 and MAC addresses into the result list.
             client = {'IP': received.psrc, 'MAC': received.hwsrc}
-            f.write(str(client) + "\n")
-            # print(client)
             result.append(client)
 
+        # If no mapping is found, write it in the dump file.
         if result == []:
             f.write("None found.\n")
 
         f.write("\n")
+
+        # Close file.
         f.close()
+
         return result
+
     except KeyboardInterrupt as e:
         sys.exit(0)
 
 def main():
-    ip = netifaces.ifaddresses('ens18')[netifaces.AF_INET][0]['addr']
-    subnet = netifaces.ifaddresses('ens18')[netifaces.AF_INET][0]['netmask']
+    # Argument Parsing
+    parser = argparse.ArgumentParser(description = "To use netsniff, please indicate the network interface to be used. The syntax is as follows:\n'sudo python3 netscniff.py -i [network interface]'", formatter_class = argparse.RawTextHelpFormatter)
+    parser.add_argument("-i", "--i", action = 'store_true', help = "Network Interface to be used")
+    args = parser.parse_args()
 
-    addr = ipcalc.IP(ip, mask=subnet)
+    try:
+        # Get the IPv4 address and subnet mask.
+        ip = netifaces.ifaddresses(str(args.i))[netifaces.AF_INET][0]['addr']
+        subnet = netifaces.ifaddresses(str(args.i))[netifaces.AF_INET][0]['netmask']
 
-    arp_scan(str(addr.guess_network()))
+        # Convert IPv4 and subnet mask to CIDR format.
+        addr = ipcalc.IP(ip, mask=subnet)
 
-    sniff.initialize()
+        # Perform ARP scan.
+        arp_scan(str(addr.guess_network()))
+
+        # Perform packet sniffing.
+        sniff.initialize()
+
+    except:
+        print("Please provide a valid network interface. Try again.")
 
 if __name__ == '__main__':
     try:
@@ -58,6 +90,7 @@ if __name__ == '__main__':
         t.daemon = True
         t.start()
         t.join()
+
     except KeyboardInterrupt as e:
         try:
             sniff.print_dump(filename)
--- sniffer.py
+++ sniffer.py
@@ -1,58 +1,74 @@
-import signal
-import socket, sys, os, re, time, netifaces
+# Author: Rafael Maderazo and Eugenio Pastoral
+# Course: Data Communications
+
+import socket, sys, netifaces
 from scapy.all import *
-import ipaddress
 from collections import *
 from threading import Thread, Event
 from time import sleep
 
+# Declare variables for network statistics.
 dhcp_count, http_count, https_count, arp_count, ftp_count, ssh_count = 0, 0, 0, 0, 0, 0
 ip_list, mac_list = [], []
 
+# This function gets the element with the most occurences in a list.
 def most_frequent(List):
     occurence_count = Counter(List)
     return occurence_count.most_common(1)[0][0]
 
+# This class sniffs the packets of the network.
 class Sniffer(Thread):
+    # This function initializes sniffer object.
     def __init__(self):
         super().__init__()
         self.daemon = True
         self.socket = None
         self.stopper = Event()
 
+    # This function starts the sniffing process of IP and ARP packets in the network.
     def run(self):
         self.socket = conf.L2listen(type=ETH_P_ALL, filter="ip or arp")
-
         sniff(opened_socket=self.socket, prn=self.check_handler, stop_filter=self.filter_stopper)
 
+    # This function categorizes the packets and updates the counter variables.
     def check_handler(self, packet):
         global dhcp_count, http_count, https_count, arp_count, ftp_count, ssh_count
+
+        # Check if the packet is a valid IP or ARP packet.
         if str(type(packet)) != "<class 'NoneType'>":
+            # Check if a MAC address is present in the packet.
             if Ether in packet:
-                srcMAC = packet[Ether].src
-                dstMAC = packet[Ether].dst
+                # Append to list.
+                mac_list.append(packet[Ether].src)
+                mac_list.append(packet[Ether].dst)
 
-                mac_list.append(srcMAC)
-                mac_list.append(dstMAC)
-
+            # Check if an IPv4 address is present in the packet.
             if IP in packet:
-                srcIP = packet[IP].src
-                dstIP = packet[IP].dst
+                # Append to list.
+                ip_list.append(packet[IP].src)
+                ip_list.append(packet[IP].dst)
 
-                ip_list.append(srcIP)
-                ip_list.append(dstIP)
-
-
+            # Check if the packet is an ARP packet and update the counter.
             if ARP in packet and (packet[ARP].op == 1 or packet[ARP].op == 2):
                 arp_count += 1
+
+            # Check if the packet is a DCHP packet and update the counter.
             elif IP in packet and (packet[IP].sport == 67 or packet[IP].dport == 68):
                 dhcp_count += 1
+
+            # Check if the packet is a FTP packet and update the counter.
             elif IP in packet and (packet[IP].dport == 20 or packet[IP].dport == 21 or packet[IP].sport == 20 or packet[IP].sport == 21):
                 ftp_count += 1
+
+            # Check if the packet is a HTTP packet and update the counter.
             elif IP in packet and (packet[IP].dport == 80 or packet[IP].sport == 80):
                 http_count += 1
+
+            # Check if the packet is a HTTPS packet and update the counter.
             elif IP in packet and (packet[IP].dport == 443 or packet[IP].sport == 443):
                 https_count += 1
+
+            # Check if the packet is a SSH packet and update the counter.
             elif IP in packet and (packet[IP].dport == 22 or packet[IP].sport == 22):
                 ssh_count += 1
 
@@ -61,14 +77,18 @@ class Sniffer(Thread):
             except:
                 print("[!] ARP packet detected.")
 
+    # This function stops the filter.
     def filter_stopper(self, packet):
         return self.stopper.isSet()
 
+    # This function joins the threads.
     def join(self, timeout=None):
         self.stopper.set()
         super().join(timeout)
 
+    # This function prints and dumps the result to the file.
     def print_dump(self, filename):
+        # Print result to console.
         print("\n\n\n[*] Sniffing stopped.\n")
         print("****Protocol Statistics:****\n")
         print("ARP Count: " + str(arp_count))
@@ -81,7 +101,10 @@ class Sniffer(Thread):
         print("\nTop IP address: " + str(most_frequent(ip_list)))
         print("Top IP address: " + str(most_frequent(mac_list)))
 
+        # Reopen the dump file/
         f = open(filename, "a")
+
+        # Append the results to the dump file.
         f.write("****Protocol Statistics****\n")
 
         f.write("ARP Count: " + str(arp_count))
@@ -95,9 +118,8 @@ class Sniffer(Thread):
         f.write("Top IP address: " + str(most_frequent(ip_list)))
         f.write("\nTop MAC address: " + str(most_frequent(mac_list)))
 
+    # This function initializes the sniffer thread.
     def initialize(self):
-        # sniffer = Sniffer()
-
         print("[*] Sniffer initialized...")
         print("***Use CTRL + C to end sniffing.***")
         sleep(2.0)
